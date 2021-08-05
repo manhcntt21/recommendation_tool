13e750c4a446ec952711e09c2eed34e9a0c5d0cb
Truong Manh <manhcntt2.1@gmail.com>
2021-08-05 17:25:29 +0700

    first commit

---
+++ app/tmdb.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+df1 = pd.read_csv('./data/recommendation_systems/tmdb_5000_credits.csv')
+df1 = pd.read_csv('./data/recommendation_systems/tmdb_5000_movies.csv')
+
+print('a')
